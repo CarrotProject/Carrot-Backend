22a908ed441337d8ec4c09039f6771fce98e1b7c
Nam-Ki-Bok <nkb7714@naver.com>
2021-08-02 17:47:17 +0900

    [2021.08.02] User Service 추가

---
+++ src/main/java/com/market/carrot/service/UserService.java
@@ -0,0 +1,19 @@
+package com.market.carrot.service;
+
+import com.market.carrot.domain.user.User;
+import com.market.carrot.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
+    }
+}
