9e4c73fa4a0f280baecfc00e43bcc2318e90c581
Nam-Ki-Bok <nkb7714@naver.com>
2021-08-03 01:00:43 +0900

    [2021.08.03] password 암호화 위한 Setter 추가

--- src/main/java/com/market/carrot/web/dto/user/UserRequestDto.java
+++ src/main/java/com/market/carrot/web/dto/user/UserRequestDto.java
@@ -3,9 +3,11 @@ package com.market.carrot.web.dto.user;
 import com.market.carrot.domain.user.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class UserRequestDto {
     private String email;
     private String password;
