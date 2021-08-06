287c7a6e132a725f8cc978f6d3e1d84d9868366f
Nam-Ki-Bok <nkb7714@naver.com>
2021-08-06 15:08:50 +0900

    [2021.08.09] 페이지 별 접근 권한 부여

--- src/main/java/com/market/carrot/config/WebSecurityConfig.java
+++ src/main/java/com/market/carrot/config/WebSecurityConfig.java
@@ -19,30 +19,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     // 인증을 무시할 경로 설정
     @Override
-    public void configure (WebSecurity webSecurity) {
-        webSecurity.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/h2-console/**");
     }
 
     @Override
-    protected void configure (HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+    protected void configure(HttpSecurity http) throws Exception {
+        http
                 .authorizeRequests()
-                .antMatchers("/").permitAll() // 누구나 접근 허용
-                .antMatchers().hasRole("USER") // USER 접근 가능
-                .antMatchers().hasRole("ADMIN") // ADMIN 접근 가능
-                .anyRequest().authenticated() // 다른 요청들은 권한에 상관없이 권한이 있어야 함
+                .antMatchers("/login", "/signup", "/user").permitAll() // 누구나 접근 허용
+                .antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
+                .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
+                .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
                 .and()
-                    .formLogin()
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/")
+                .formLogin()
+                .loginPage("/login") // 로그인 페이지 링크
+                .defaultSuccessUrl("/") // 로그인 성공 후 리다이렉트 주소
                 .and()
-                    .logout()
-                        .logoutSuccessUrl("/login")
-                        .invalidateHttpSession(true);
+                .logout()
+                .logoutSuccessUrl("/login") // 로그아웃 성공시 리다이렉트 주소
+                .invalidateHttpSession(true) // 세션 날리기
+        ;
     }
 
     @Override
-    public void configure (AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         // 패스워드 암호화
         authenticationManagerBuilder.userDetailsService(userService)
                 .passwordEncoder(new BCryptPasswordEncoder());
