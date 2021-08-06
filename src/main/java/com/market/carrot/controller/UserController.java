00a27dfaff031ae97c264325d12edd80f8cac87f
Nam-Ki-Bok <nkb7714@naver.com>
2021-08-06 15:07:31 +0900

    [2021.08.09] 로그아웃 추가

--- src/main/java/com/market/carrot/controller/UserController.java
+++ src/main/java/com/market/carrot/controller/UserController.java
@@ -3,8 +3,14 @@ package com.market.carrot.controller;
 import com.market.carrot.service.UserService;
 import com.market.carrot.web.dto.user.UserRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @Controller
@@ -12,11 +18,17 @@ public class UserController {
     private final UserService userService;
 
     // 회원가입 임시 주소
-    @PostMapping("/signUp")
+    @PostMapping("/user")
     public String signUp(UserRequestDto userRequestDto) {
         userService.save(userRequestDto);
 
         // 회원 가입 후 리다이렉트 임시 주소
+        return "redirect:/login";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
