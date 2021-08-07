9942e7aef0ceaa5a5d6dec7735cabf4d9dcd9eb5
Nam-Ki-Bok <nkb7714@naver.com>
2021-08-07 22:37:45 +0900

    [2021.08.07] Post Repository 추가

---
+++ src/main/java/com/market/carrot/domain/posts/PostsRepository.java
@@ -0,0 +1,11 @@
+package com.market.carrot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT post FROM Posts post ORDER BY post.id DESC")
+    List<Posts> findAllDesc();
+}
